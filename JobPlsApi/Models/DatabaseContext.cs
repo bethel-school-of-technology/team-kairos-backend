9b158ae1ab0c263780c69d989c151ea1487a837a
Mario Gonzales <mariog.2822@gmail.com>
2022-06-08 20:26:40 -0700

    beginning stages of JWT and Spec User CRUD

---
+++ JobPlsApi/Models/DatabaseContext.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+
+
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<RecruiterInfo>? RecruiterInfo { get; set; }
+        public virtual DbSet<JobseekerInfo>? JobseekerInfo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RecruiterInfo>(entity =>
+            {
+                entity.HasNoKey();
+                entity.ToTable("RecruiterInfo");
+                entity.Property(e => e.UserId).HasColumnName("UserId");
+                entity.Property(e => e.DisplayName).HasMaxLength(60).IsUnicode(false);
+                entity.Property(e => e.UserName).HasMaxLength(30).IsUnicode(false);
+                entity.Property(e => e.Email).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.Password).HasMaxLength(20).IsUnicode(false);
+                entity.Property(e => e.CreatedDate).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<JobseekerInfo>(entity =>
+            {
+                entity.ToTable("JobseekerInfo");
+                entity.Property(e => e.EmployeeID).HasColumnName("EmployeeID");
+                entity.Property(e => e.NationalIDNumber).HasMaxLength(15).IsUnicode(false);
+                entity.Property(e => e.EmployeeName).HasMaxLength(100).IsUnicode(false);
+                entity.Property(e => e.LoginID).HasMaxLength(256).IsUnicode(false);
+                entity.Property(e => e.JobTitle).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.BirthDate).IsUnicode(false);
+                entity.Property(e => e.MaritalStatus).HasMaxLength(1).IsUnicode(false);
+                entity.Property(e => e.Gender).HasMaxLength(1).IsUnicode(false);
+                entity.Property(e => e.HireDate).IsUnicode(false);
+                entity.Property(e => e.VacationHours).IsUnicode(false);
+                entity.Property(e => e.SickLeaveHours).IsUnicode(false);
+                entity.Property(e => e.RowGuid).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.ModifiedDate).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }

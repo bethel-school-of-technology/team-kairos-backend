04da19efd1df9077fc1661cd90c4002d6ee9d6d2
Mario Gonzales <mariogonzales@Marios-MacBook-Pro.local>
2022-06-23 20:28:40 -0700

    last minute changes

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -21,6 +21,35 @@ public class UsersController : ControllerBase
         _userService = userService;
         _context =context;
     }
+    [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(long id)
+        {
+          if (_context.User == null)
+          {
+              return NotFound();
+          }
+            var user = await _context.User.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+    [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+          if (_context.User == null)
+          {
+              return Problem("Entity set 'AuthenticationDbContext.User'  is null.");
+          }
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
 
     [HttpPost("authenticate")]
     public async Task<IActionResult> Authenticate([FromBody] User AuthUser, [FromRoute] AuthenticateRequest model)
